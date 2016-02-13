6244883ffadaf474565f944bac5319d01933ea34
Rémi Deltombe <deltombe.remi@gmail.com>
2016-02-13 13:39:50 +0100

    Dev option
    Add reflector removing detection

--- options/dev.js
+++ options/dev.js
@@ -2,10 +2,23 @@ var reflectorFactory = require('./../src/model/reflector/factory.js');
 
 module.exports = function(path){
 	setInterval(function(){
-		var i,data = require(path);
+		var i,data = require(path), existing=[], toDelete=[];
 		for (i in data) {
+			existing.push(data[i].reflector);
 			reflectorFactory.get(data[i].reflector).populate(data[i]);
 		}
-		delete require.cache[require.resolve(path)]
+
+		reflectorFactory.each(function(index, reflector){
+			if (existing.indexOf(reflector.id)===-1) {
+				toDelete.push(reflector.id);
+			}
+		});
+
+		for (i in toDelete) {
+			console.log('remove: '+toDelete[i]);
+			reflectorFactory.remove(toDelete[i]);
+		}
+
+		delete require.cache[require.resolve(path)];
 	},2000);
 }
